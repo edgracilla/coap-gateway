f6f15105ad984fc299676fd6cea11271ad5d8cce
Benj Sicam <bsicam@reekoh.com>
2016-04-28 16:20:40 +0800

    Updated to make use of the new requestDeviceInfo API for a more scalable device information storage.

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
 'use strict';
 
-var async             = require('async'),
-	platform          = require('./platform'),
-	isEmpty           = require('lodash.isempty'),
-	clients           = {},
-	authorizedDevices = {},
+var async    = require('async'),
+	platform = require('./platform'),
+	isEmpty  = require('lodash.isempty'),
+	clients  = {},
 	server;
-
-platform.on('adddevice', function (device) {
-	if (!isEmpty(device) && !isEmpty(device._id)) {
-		authorizedDevices[device._id] = device;
-		platform.log(`Successfully added ${device._id} to the pool of authorized devices.`);
-	}
-	else
-		platform.handleException(new Error(`Device data invalid. Device not added. ${device}`));
-});
-
-platform.on('removedevice', function (device) {
-	if (!isEmpty(device) && !isEmpty(device._id)) {
-		delete authorizedDevices[device._id];
-		platform.log(`Successfully added ${device._id} from the pool of authorized devices.`);
-	}
-	else
-		platform.handleException(new Error(`Device data invalid. Device not removed. ${device}`));
-});
 
 platform.once('close', function () {
 	let d = require('domain').create();
@@ -43,13 +24,10 @@ platform.once('close', function () {
 	});
 });
 
-platform.once('ready', function (options, registeredDevices) {
+platform.once('ready', function (options) {
 	let keyBy  = require('lodash.keyby'),
 		config = require('./config.json'),
 		coap   = require('coap');
-
-	if (!isEmpty(registeredDevices))
-		authorizedDevices = keyBy(registeredDevices, '_id');
 
 	if (isEmpty(options.data_url))
 		options.data_url = config.data_url.default;
@@ -71,53 +49,61 @@ platform.once('ready', function (options, registeredDevices) {
 		], (error, payloadObj) => {
 			if (error || isEmpty(payloadObj.device)) return platform.handleException(new Error('Invalid data sent. Data must be a valid JSON String with at least a "device" field which corresponds to a registered Device ID.'));
 
-			if (isEmpty(authorizedDevices[payloadObj.device])) {
-				return platform.log(JSON.stringify({
-					title: 'CoAP Gateway - Access Denied. Unauthorized Device',
-					device: payloadObj.device
-				}));
-			}
+			platform.requestDeviceInfo(payloadObj.device, (error, requestId) => {
+				setTimeout(() => {
+					platform.removeAllListeners(requestId);
+				}, 5000);
 
-			let url = request.url.split('/')[1];
+				platform.once(requestId, (deviceInfo) => {
+					if (isEmpty(deviceInfo)) {
+						return platform.log(JSON.stringify({
+							title: 'CoAP Gateway - Access Denied. Unauthorized Device',
+							device: payloadObj.device
+						}));
+					}
 
-			if (url === options.data_url) {
-				platform.processData(payloadObj.device, payload);
+					let url = request.url.split('/')[1];
 
-				platform.log(JSON.stringify({
-					title: 'CoAP Gateway - Data Received',
-					device: payloadObj.device,
-					data: payloadObj
-				}));
+					if (url === options.data_url) {
+						platform.processData(payloadObj.device, payload);
 
-				if (isEmpty(clients[payloadObj.device]))
-					clients[payloadObj.device] = response;
-			}
-			else if (url === options.message_url) {
-				if (isEmpty(payloadObj.target) || isEmpty(payloadObj.message)) return platform.handleException(new Error('Invalid message or command. Message must be a valid JSON String with "target" and "message" fields. "target" is a registered Device ID. "message" is the payload.'));
+						platform.log(JSON.stringify({
+							title: 'CoAP Gateway - Data Received',
+							device: payloadObj.device,
+							data: payloadObj
+						}));
 
-				platform.sendMessageToDevice(payloadObj.target, payloadObj.message);
+						if (isEmpty(clients[payloadObj.device]))
+							clients[payloadObj.device] = response;
+					}
+					else if (url === options.message_url) {
+						if (isEmpty(payloadObj.target) || isEmpty(payloadObj.message)) return platform.handleException(new Error('Invalid message or command. Message must be a valid JSON String with "target" and "message" fields. "target" is a registered Device ID. "message" is the payload.'));
 
-				platform.log(JSON.stringify({
-					title: 'CoAP Gateway - Message Received',
-					source: payloadObj.device,
-					target: payloadObj.target,
-					message: payloadObj.message
-				}));
-			}
-			else if (url === options.groupmessage_url) {
-				if (isEmpty(payloadObj.target) || isEmpty(payloadObj.message)) return platform.handleException(new Error('Invalid group message or command. Group messages must be a valid JSON String with "target" and "message" fields. "target" is a device group id or name. "message" is the payload.'));
+						platform.sendMessageToDevice(payloadObj.target, payloadObj.message);
 
-				platform.sendMessageToGroup(payloadObj.target, payloadObj.message);
+						platform.log(JSON.stringify({
+							title: 'CoAP Gateway - Message Received',
+							source: payloadObj.device,
+							target: payloadObj.target,
+							message: payloadObj.message
+						}));
+					}
+					else if (url === options.groupmessage_url) {
+						if (isEmpty(payloadObj.target) || isEmpty(payloadObj.message)) return platform.handleException(new Error('Invalid group message or command. Group messages must be a valid JSON String with "target" and "message" fields. "target" is a device group id or name. "message" is the payload.'));
 
-				platform.log(JSON.stringify({
-					title: 'CoAP Gateway - Group Message Received',
-					source: payloadObj.device,
-					target: payloadObj.target,
-					message: payloadObj.message
-				}));
-			}
-			else
-				platform.handleException(new Error(`Invalid url specified. URL: ${url}`));
+						platform.sendMessageToGroup(payloadObj.target, payloadObj.message);
+
+						platform.log(JSON.stringify({
+							title: 'CoAP Gateway - Group Message Received',
+							source: payloadObj.device,
+							target: payloadObj.target,
+							message: payloadObj.message
+						}));
+					}
+					else
+						platform.handleException(new Error(`Invalid url specified. URL: ${url}`));
+				});
+			});
 		});
 	});
 
